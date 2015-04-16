d35109fd13f665c88071425f830fd6f591def903
Mike <mpatrick@cds-global.com>
2015-04-16 10:06:22 -0500

    Initial commit.

---
+++ src/com/cds/web/test/servlet/AjaxServlet.java
@@ -0,0 +1,41 @@
+package com.cds.web.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjaxServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		doPost(request, response);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		StringBuilder sb = new StringBuilder();
+		String token = request.getParameter("OWASP-CSRFTOKEN");
+		Enumeration<String> params = request.getParameterNames();
+		System.out.println("params:");
+		
+		while (params.hasMoreElements())
+		{
+			System.out.println(params.nextElement());
+		}
+		System.out.println("headers:");
+		Enumeration<String> headers = request.getHeaderNames();
+		while (headers.hasMoreElements())
+		{
+			System.out.println(headers.nextElement());
+		}
+		sb.append("Content from ajax servlet\n");
+		sb.append(token);
+		PrintWriter pw = response.getWriter();
+		pw.println(sb.toString());
+		
+	}
+}
---
+++ src/com/cds/web/test/servlet/ProcessServlet.java
@@ -0,0 +1,37 @@
+package com.cds.web.test.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cds.web.test.domain.DomainObject;
+
+public class ProcessServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -4027458989452089813L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	{
+		throw new RuntimeException("No GETting!");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		DomainObject dob = new DomainObject();
+		dob.setName("frank");
+		dob.setAddress("123 Main St");
+		request.setAttribute("domainObjects", dob);
+		
+		if (username.length() > 0 && password.length() > 0)
+			request.getRequestDispatcher("/success.jsp").forward(request, response);
+		else
+			response.sendRedirect("/TestWebProject/failure.jsp");
+		
+	}
+}
